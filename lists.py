d956ce91203632898e1cccf5d674fdb494713244
OldFredddy <Freddyforever1989>
2023-02-20 02:54:39 +0300

    page 51

---
+++ lists.py
@@ -0,0 +1,6 @@
+bicucles = ['bike', 'cannondale', 'redline', 'specialized']
+print(bicucles)
+print(bicucles[0])
+print(bicucles[1])
+print(bicucles[2])
+print(bicucles[3])
--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Start python learning")
-print("sdad")
+first_name = "NAME"
+last_name = "Surname"
+
+# full_name = first_name + last_name
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+
+
+notStripName = "     STRIIIP"
+print(f"\n{notStripName.lstrip()}")
+
+
+message = 'dfsaf "ghbdn"  '
+print(message)
