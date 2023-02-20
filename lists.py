0b380c49f24f749b35f35e3046f5fdfc7d1c365d
OldFredddy <Freddyforever1989>
2023-02-21 02:18:14 +0300

    page 56

--- lists.py
+++ lists.py
@@ -4,3 +4,15 @@ print(bicucles[0])
 print(bicucles[1])
 print(bicucles[2])
 print(bicucles[3])
+
+bicucles.append('123')
+print(bicucles)
+
+bicucles.insert(0, '55555')
+print(bicucles)
+
+del bicucles[0]
+print(bicucles)
+poppet_bicucle = bicucles.pop()
+print(bicucles)
+print(poppet_bicucle)
